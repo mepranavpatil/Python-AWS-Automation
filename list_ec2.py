bca6f0fc846bcd05b5f07dee37c1e505d7431068
mepranavpatil <mepranavpatil@gmail.com>
2026-08-18 16:19:18 +0530

    print Only Reservation Count

--- list_ec2.py
+++ list_ec2.py
@@ -1,9 +1,7 @@
 import boto3
 
-# Create EC2 client
 ec2 = boto3.client("ec2")
 
-# Fetch EC2 information
 response = ec2.describe_instances()
 
-print(response)
+print("Reservations Found:", len(response["Reservations"]))
